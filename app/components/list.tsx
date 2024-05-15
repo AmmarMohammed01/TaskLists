fe7d4cb392bd8cd138ef52a59a269fa0f2cf2cec
Ammar Mohammed <ammar.hm321@gmail.com>
2024-05-14 19:55:55 -0500

    before change to form

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -1,12 +1,15 @@
 'use client';
+//https://react.dev/reference/react-dom/components/input
+//https://react.dev/reference/react-dom/components/form
 import { useState } from "react";
 
 export default function List() {
-  const [list, setList] = useState();
+  const [list, setList] = useState("");
 
   return (
     <>
-      <input placeholder="Type Time and Task Here"></input>
+      <p>{list}</p>
+      <input placeholder="Type Time and Task Here" onChange={e => setList(e.target.value)}></input>
       <button>Submit</button>
     </>
   );
